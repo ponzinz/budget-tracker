3a83073790aefcf15af7e4c9f75441c429d73b73
ponzinz <simonegalaxynexus@gmail.com>
2024-10-06 19:24:21 +0200

    feat: add router with routes and functions to view requested pages or abort with 404 error

---
+++ router.php
@@ -0,0 +1,30 @@
+<?php
+
+// get only the uri path from array created by parse_url function
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+// if the route exists go to the page, else abort
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+routeToController($uri, $routes);
